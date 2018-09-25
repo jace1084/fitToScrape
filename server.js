2cf1211e471d23fc3311fbdf51191bd05861e06b
jace <jace.sf84@gmail.com>
2018-09-25 16:55:55 -0700

    added request and initial html load

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 var request = require("request");
 var cheerio = require("cheerio");
 
+
+request("https://www.indeed.com/jobs?q=&l=San+Francisco%2C+CA", (err, response, html) => {
+    const $ = cheerio.load(html);
+    const results = [];
+
+    $("").each((i, el) => {
+        const title = $(el).text();
+    })
+})
+
