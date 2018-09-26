8ccbc5ad10686dc4d9f3eb8e7bc47cc937b16e0f
jace <jace.sf84@gmail.com>
2018-09-25 19:29:21 -0700

    commented out company due to error in scrape

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ request("https://www.indeed.com/jobs?q=&l=San+Francisco%2C+CA", (err, response, 
     const $ = cheerio.load(html);
     const results = [];
 
-    $("a.turnstileLink").each((i, el) => {
+    $("a.jobtitle.turnstileLink").each((i, el) => {
         const title = $(el).text();
-        const company = $("span.company").text().replace(/\s\s+/g, "");
+        // const company = $("span.company").text().replace(/\s\s+/g, "");
         const location = $("span.location").text().replace(/\s\s+/g, "");
         const summary = $("span.summary").text().replace(/\s\s+/g, "");
         const link = $(el).attr("href");
 
         results.push({
             title: title,
-            company: company,
+            // company: company,
             link: link,
             location: location,
             summary: summary
