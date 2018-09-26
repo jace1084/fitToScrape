f81fb8c177e76fd49a6c9fbc94f720371a1fc783
jace <jace.sf84@gmail.com>
2018-09-25 22:03:36 -0700

    correction

---
+++ models/article.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var Note = require("./notes");
+
+console.log('Running mongoose version: ', mongoose.version);
+ 
+const ArticleSchema = mongoose.Schema({
+    headline: {
+        type: String,
+    },
+    link: {
+        type: String,
+    },
+    notes: [{ type: Schema.Types.ObjectId, ref: 'Notes' }],
+    date:   {  
+        type: Date, 
+        default: Date.now  
+    },
+    location: {
+        type: String,
+    }
+});
+
+
+console.log('ArticleSchema created!');
+
+var Article = mongoose.model('Article', ArticleSchema);
+module.exports = Article;
