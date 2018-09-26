e92af2ac92c08e8c31b172e6642d82006a193d6b
jace <jace.sf84@gmail.com>
2018-09-25 19:59:31 -0700

    moved server.js content to scrape.js

---
+++ routes/scrape.js
@@ -0,0 +1,26 @@
+var request = require("request");
+var cheerio = require("cheerio");
+
+
+request("https://www.indeed.com/jobs?q=&l=San+Francisco%2C+CA", (err, response, html) => {
+    const $ = cheerio.load(html);
+    const results = [];
+
+    $("a.jobtitle.turnstileLink").each((i, el) => {
+        const title = $(el).text();
+        // const company = $("span.company").text().replace(/\s\s+/g, "");
+        const location = $("span.location").text().replace(/\s\s+/g, "");
+        const summary = $("span.summary").text().replace(/\s\s+/g, "");
+        const link = $(el).attr("href");
+
+        results.push({
+            headline: title,
+            // company: company,
+            link: link,
+            location: location,
+            summary: summary
+        });
+    });
+    console.log(results);
+})
+
