914fadd6c0a7df36917271bc3b9b3a405c1b7654
jace <jace.sf84@gmail.com>
2018-09-25 20:02:19 -0700

    added dependancies to server.js

--- server.js
+++ server.js
@@ -1,26 +1,8 @@
-var request = require("request");
-var cheerio = require("cheerio");
-
-
-request("https://www.indeed.com/jobs?q=&l=San+Francisco%2C+CA", (err, response, html) => {
-    const $ = cheerio.load(html);
-    const results = [];
-
-    $("a.jobtitle.turnstileLink").each((i, el) => {
-        const title = $(el).text();
-        // const company = $("span.company").text().replace(/\s\s+/g, "");
-        const location = $("span.location").text().replace(/\s\s+/g, "");
-        const summary = $("span.summary").text().replace(/\s\s+/g, "");
-        const link = $(el).attr("href");
-
-        results.push({
-            title: title,
-            // company: company,
-            link: link,
-            location: location,
-            summary: summary
-        });
-    });
-    console.log(results);
-})
-
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const request = require("request");
+const cheerio = require("cheerio");
+//const logger = require("logger");
+const app = express();
