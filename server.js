95d88e7784a98f8414cc7d400804aadbcb55d523
jace <jace.sf84@gmail.com>
2018-09-25 17:55:36 -0700

    adding in items I want to scrape from the site

--- server.js
+++ server.js
@@ -8,11 +8,17 @@ request("https://www.indeed.com/jobs?q=&l=San+Francisco%2C+CA", (err, response, 
 
     $("a.turnstileLink").each((i, el) => {
         const title = $(el).text();
-        const link = $(el).children().attr("href");
+        const company = $("span.company").text().replace(/\s\s+/g, "");
+        const location = $("span.location").text().replace(/\s\s+/g, "");
+        const summary = $("span.summary").text().replace(/\s\s+/g, "");
+        const link = $(el).attr("href");
 
         results.push({
             title: title,
-            link: link
+            company: company,
+            link: link,
+            location: location,
+            summary: summary
         });
     });
     console.log(results);
