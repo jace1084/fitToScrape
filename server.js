b5c3d57dbcbd490e683561d7161e6b613d1141dc
jace <jace.sf84@gmail.com>
2018-10-16 22:17:11 -0700

    fixing some code and going to mongojs

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const mongoose = require("mongoose");
+var mongojs = require("mongojs");
+// const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const request = require("request");
 var cheerio = require("cheerio");
@@ -17,33 +18,42 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-// var databaseUrl = "scraper";
-// var collections = ["articles"];
+var databaseUrl = "scraper";
+var collections = ["articles"];
+
+// Use mongojs to hook the database to the db variable
+var db = mongojs(databaseUrl, collections);
+
+// This makes sure that any errors are logged if mongodb runs into an issue
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
 
 app.use(express.static("./public"));
 
-mongoose.Promise = Promise;
+// mongoose.Promise = Promise;
 
 const dbURI = process.env.MONGODB_URI || "mongodb://localhost:27017/jobScrape";
 
-// Database configuration with mongoose
-mongoose.connect(dbURI);
+// // Database configuration with mongoose
+// mongoose.connect(dbURI);
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
-// Show any mongoose errors
-db.on("error", function(error) {
-    console.log("Mongoose Error: ", error);
-});
+// // Show any mongoose errors
+// db.on("error", function(error) {
+//     console.log("Mongoose Error: ", error);
+// });
 
-// Once logged in to the db through mongoose, log a success message
-db.once("open", function() {
-    console.log("Mongoose connection successful.");
-    // start the server, listen on port 3000
-    app.listen(PORT, function() {
-        console.log("App running on port" + PORT);
-    })
-});
+// // Once logged in to the db through mongoose, log a success message
+// db.once("open", function() {
+//     console.log("Mongoose connection successful.");
+//     // start the server, listen on port 3000
+//     app.listen(PORT, function() {
+//         console.log("App running on port" + PORT);
+//     })
+// });
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({
